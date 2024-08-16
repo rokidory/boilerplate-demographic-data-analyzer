77fcf041ef60e66bc26fae96a9d443bd6ef6ed47
roki <151161348+rokidory@users.noreply.github.com>
2024-08-16 15:37:09 +0700

    began updating demographic_data_analyzer.py

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -3,13 +3,13 @@ import pandas as pd
 
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    df = None
+    df = pd.read_csv('adult.data.csv')
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = None
+    race_count = df['race'].unique()
 
     # What is the average age of men?
-    average_age_men = None
+    average_age_men = df.loc[df['sex'] == 'Male', 'age'].mean()
 
     # What is the percentage of people who have a Bachelor's degree?
     percentage_bachelors = None
